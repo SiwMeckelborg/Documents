55abe640b0cbe2c546f74af634a1694cf96c3307
Siw Midtgård Meckelborg <siw.meckelborg@edisys.no>
2019-04-15 15:23:43 +0200

    test

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,70 +1,61 @@
-var lunrIndex, pagesIndex;
+(function() {
+  function displaySearchResults(results, store) {
+    var searchResults = document.getElementById('search-results');
 
-function endsWith(str, suffix) {
-    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-}
+    if (results.length) { // Are there any results?
+      var appendString = '';
 
-// Initialize lunrjs using our generated index file
-function initLunr() {
-    if (!endsWith(baseurl,"/")){
-        baseurl = baseurl+'/'
-    };
+      for (var i = 0; i < results.length; i++) {  // Iterate over the results
+        var item = store[results[i].ref];
+        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
+      }
 
-    // First retrieve the index file
-    $.getJSON(baseurl +"lunr-index.json")
-        .done(function(data) {
-            pagesIndex = data.meta;
-            lunrIndex = lunr.Index.load(data.index);
-        })
-        .fail(function(jqxhr, textStatus, error) {
-            var err = textStatus + ", " + error;
-            console.error("Error getting lunr-index.json: ", err);
-        });
-}
+      searchResults.innerHTML = appendString;
+    } else {
+      searchResults.innerHTML = '<li>No results found</li>';
+    }
+  }
 
-/**
- * Trigger a search in lunr and transform the result
- *
- * @param  {String} query
- * @return {Array}  results
- */
-function search(query) {
-    // Find the item in our index corresponding to the lunr one to have more info
-    return lunrIndex.search(query).map(function(result) {
-            return pagesIndex.filter(function(page) {
-                return page.uri === result.ref;
-            })[0];
-        });
-}
+  function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
 
-// Let's get started
-initLunr();
-$( document ).ready(function() {
-    var horseyList = horsey($("#search-by").get(0), {
-        suggestions: function (value, done) {
-            var query = $("#search-by").val();
-            var results = search(query);
-            done(results);
-        },
-        filter: function (q, suggestion) {
-            return true;
-        },
-        set: function (value) {
-            location.href=value.uri;
-        },
-        render: function (li, suggestion) {
-            var uri = suggestion.uri.substring(1,suggestion.uri.length);
+    for (var i = 0; i < vars.length; i++) {
+      var pair = vars[i].split('=');
 
-            suggestion.href = baseurl + uri;
+      if (pair[0] === variable) {
+        return decodeURIComponent(pair[1].replace(/\+/g, '%20'));
+      }
+    }
+  }
 
-            var query = $("#search-by").val();
-            var numWords = 2;
-            var text = suggestion.content.match("(?:\\s?(?:[\\w]+)\\s?){0,"+numWords+"}"+query+"(?:\\s?(?:[\\w]+)\\s?){0,"+numWords+"}");
-            suggestion.context = text;
-            var image = '<div>' + '» ' + suggestion.title + '</div><div style="font-size:12px">' + (suggestion.context || '') +'</div>';
-            li.innerHTML = image;
-        },
-        limit: 10
+  var searchTerm = getQueryVariable('query');
+
+  if (searchTerm) {
+    document.getElementById('search-box').setAttribute("value", searchTerm);
+
+    // Initalize lunr with the fields it will be searching on. I've given title
+    // a boost of 10 to indicate matches on this field are more important.
+    var idx = lunr(function () {
+      this.field('id');
+      this.field('title', { boost: 10 });
+      this.field('author');
+      this.field('category');
+      this.field('content');
     });
-    horseyList.refreshPosition();
-});
+
+    for (var key in window.store) { // Add the data to lunr
+      idx.add({
+        'id': key,
+        'title': window.store[key].title,
+        'author': window.store[key].author,
+        'category': window.store[key].category,
+        'content': window.store[key].content
+      });
+
+      var results = idx.search(searchTerm); // Get lunr to perform a search
+      displaySearchResults(results, window.store); // We'll write this in the next section
+    }
+  }
+})();
